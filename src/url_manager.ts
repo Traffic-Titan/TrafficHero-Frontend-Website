d649e0e5fbccd2600fa0bbdda977d6780b0bbb05
kentlin0919 <kent900919@gmail.com>
2023-10-06 13:57:10 +0800

    feat: 新增api CRUD

--- src/url_manager.ts
+++ src/url_manager.ts
@@ -1,4 +1,5 @@
-let url_login : string = "/Website/Account/Login";
-""
-
-export{url_login}
+let url_login: string = "/Website/Account/Login";
+let url_register: string = "/Website/Account/Register";
+let uri_googlesso:string = '/Website/Account/GoogleSSO'
+let url_ChangePassword: string = "/Website/Account/ChangePassword"
+export { url_login, url_register }
