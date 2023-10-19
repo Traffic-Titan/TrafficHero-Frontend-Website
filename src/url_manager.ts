68d5e0738a368d437a349b5a3591fcd0c25522b1
kentlin0919 <kent900919@gmail.com>
2023-10-19 09:46:07 +0800

    fix: build

--- src/url_manager.ts
+++ src/url_manager.ts
@@ -1,7 +1,6 @@
 let url_login: string = "/Website/Account/Login";
 let url_register: string = "/Website/Account/Register";
-let uri_googlesso: string = '/Website/Account/GoogleSSO'
-let url_ChangePassword: string = "/Website/Account/ChangePassword"
+
 
 
 
