2e8b69e2e628d361db5dc3585f505c090d561ecf
kentlin0919 <96437895+kentlin0919@users.noreply.github.com>
2023-10-19 15:04:45 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,5 @@
-// const express = require('express');
-// const path = require('path');
-// const app = express();
-
-// // 將Vue.js應用的靜態資源提供給客戶端
-// app.use(express.static(path.join(__dirname, 'dist')));
-
-// // 所有路由導向到index.html
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-// });
-
-// const port = process.env.PORT || 3000;
-// app.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
-
-
-import express from 'express';
-import path from 'path';
-
+const express = require('express');
+const path = require('path');
 const app = express();
 
 // 將Vue.js應用的靜態資源提供給客戶端
@@ -33,3 +14,22 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+
+// import express from 'express';
+// import path from 'path';
+
+// const app = express();
+
+// // 將Vue.js應用的靜態資源提供給客戶端
+// app.use(express.static(path.join(__dirname, 'dist')));
+
+// // 所有路由導向到index.html
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+// });
+
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
